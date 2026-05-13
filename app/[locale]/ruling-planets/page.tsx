c93e3fea8e546f7c3f28f98654fe5a9bb79f6c30
Alisa Sosunova <alisasosunova@MacBook-Pro-Alisa.local>
2026-05-13 21:38:50 +0300

    fix bugs

--- app/[locale]/ruling-planets/page.tsx
+++ app/[locale]/ruling-planets/page.tsx
@@ -8,14 +8,14 @@ export default function RulingPlanetsPage() {
     const t = useTranslations('Topics.rulingPlanets');
     const locale = useLocale();
     return (
-        <main className="min-h-screen p-8">
-            <div className="flex-1 px-12 py-12 max-w-7xl mx-auto w-full">
+        <main className="min-h-screen p-4 md:p-8">
+            <div className="flex-1 px-4 py-4 md:px-12 md:py-12 max-w-7xl mx-auto w-full">
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
                     <div className="lg:col-span-8 space-y-12">
                         <div className="lg:col-span-8 space-y-12">
                             <div className="bg-secondary p-10 pl-12 rounded-[40px] relative overflow-hidden group border border-transparent">
-                                <Moon className="absolute top-[-20px] right-[-20px] w-24 h-24 text-gold text-text-light/70 animate-pulse" />
-                                <h3 className="text-3xl font-serif mt-2 mb-4 text-dark">
+                                <Moon className="absolute top-[-10px] right-[-10px] md:top-[-20px] md:right-[-20px] w-12 h-12 md:w-32 md:h-32 text-gold text-text-light/70 animate-pulse" />
+                                <h3 className="text-2xl md:text-3xl font-serif mt-2 mb-4 text-dark">
                                     {t.rich('title', {
                                         br: () => <br />
                                     })}
